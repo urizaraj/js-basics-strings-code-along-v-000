19872d48947e253b11ddf5f228805c04943c4f9b
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 18:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
